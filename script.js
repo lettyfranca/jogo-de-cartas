2f1d738243f123130f5c5f7376f76940eeea5039
lettyfranca <leeffrance@gmail.com>
2022-07-21 00:27:22 -0300

    uso do array

--- script.js
+++ script.js
@@ -2,3 +2,10 @@
  * Jogo de Cartas c/ Array
  * @author Letícia França
  */
+
+let nipes = ['♥','♦','♣','♠'];
+let faces = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
+let nipe = nipes[Math.floor(Math.random()*4)];
+let face = faces[Math.floor(Math.random()*faces.length)];
+
+document.write("<h1>" + face + nipe + "</h1>");
